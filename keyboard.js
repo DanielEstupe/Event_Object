e3266ed05b5de86cdc0f34e05ec77c1572242f03
Daniel Estupe <DanielEstupe@users.noreply.github.com>
2016-10-10 20:42:57 -0600

    Update keyboard.js

--- keyboard.js
+++ keyboard.js
@@ -1,1 +1,11 @@
-alert("hola");
+
+function listenKeyboard(_event) {
+  var evento = _event || window.event;
+  var letra = evento.charCode || evento.keyCode;
+  var letra = String.fromCharCode(letra);
+  console.log(letra);
+}
+
+window.onload = function() {
+  document.onkeypress = listenKeyboard;
+};
